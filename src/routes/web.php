c58318b255f9fde2df9dfbf09c2ebeb4241a1c46
shion <shion48691811@gmail.com>
2025-10-17 21:53:08 +0900

    [add] ルーティング処理

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,37 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // topページで使います
+    return view('index');
+});
+Route::get('/ai-research', function () {
+    // ai-researchページで使います
+    return view('ai-research');
+});
+Route::get('/ai-research-category', function () {
+    // ai-research-categoryページで使います
+    return view('ai-research-category');
+});
+Route::get('/search-list', function () {
+    // search-listページで使います
+});
+Route::get('/detail', function () {
+    // detailページで使います
+    return view('detail');
+});
+Route::get('/favorite-list', function () {
+    // favorite-listページで使います
+    return view('favorite');
+});
+Route::get('/login', function () {
+    // loginページで使います
+    return view('login');
+});
+Route::get('/signup', function () {
+    // signupページで使います
+    return view('signup');
+});
+Route::get('/profile', function () {
+    // profileページで使います
+    return view('profile');
 });
