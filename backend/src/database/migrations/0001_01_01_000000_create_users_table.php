c3aba5732382c79ad5e5184defa2e54f104d3535
asano <asano.s.24kdgn@gmail.com>
2025-10-27 18:17:03 +0900

    [add]ユーザー登録機能を実装 (#9)

--- backend/src/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('password')->unique();
+            $table->decimal('current_lat');
+            $table->decimal('current_lon');
             $table->rememberToken();
             $table->timestamps();
         });
