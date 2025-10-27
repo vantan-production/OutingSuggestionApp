ab1cccbee1b66fc13dba3cd1c14317d2df6db816
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-10-27 20:39:31 +0900

    [add]周辺店舗取得機能を追加

--- backend/src/routes/api.php
+++ backend/src/routes/api.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
+use App\Http\Controllers\PlacesController;
 
 // テスト用のシンプルなAPI
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working!']);
 });
 
-// 天気API
+// 天気情報取得
 Route::get('/weather', [WeatherController::class, 'getWeather']);
+
+// 店舗情報取得
+// 一覧
+Route::get('/places/nearby', [PlacesController::class, 'searchNearby']);
