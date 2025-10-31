487e053156aa16718d0e6787bffab680e15590a1
ito <yuma.24s1100609@gmail.com>
2025-10-31 11:42:28 +0900

    add 写真作成

---
+++ frontend/src/components/store-info.tsx
@@ -0,0 +1,12 @@
+export function StoreInfo () {
+    return (
+        <div className=" w-44 h-44">
+            <img className=" w-44 h-24" src="/images/shop_01.jpeg" alt="store-photo" />
+            <div>
+                <p></p>
+            </div>
+        </div>
+    )
+};
+
+export default StoreInfo;
