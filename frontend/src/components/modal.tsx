a2bf31c408097ba1b9ea52bf9c6d13f295630658
shion <shion48691811@gmail.com>
2025-11-07 17:14:29 +0900

    モーダルの微調整

--- frontend/src/components/modal.tsx
+++ frontend/src/components/modal.tsx
@@ -1,23 +1,21 @@
 `use client`
-import { useState } from "react";
 
-function Modal(props: { title: string}) {
-    const [isOpen, setIsOpen] = useState(true);
-    const handleClose = () => {
-        setIsOpen(false);
-    }
-    if (!isOpen) return null;
+function Modal(props: { onClose: () => void }) {
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div>
-                <h2 className="h2-bold text-black">{props.title}</h2>
-                <p className="p text-black">全ての機能を利用するにはログインが必要です</p>
-                <button>
-                    <a href="/login" className="bg-blue text-white rounded-3 p-2 p w-30 h-10">ログイン</a>
-                </button>
-                <button onClick={handleClose}>
-                    <p className="p text-black bg-orange rounded-3 p-2 w-30 h-10">このまま続ける</p>
-                </button>
+        <div className="fixed inset-0 bg-black bg-opacity-0 flex justify-center items-center" onClick={props.onClose}>
+            <div className="bg-beige rounded-3 px-4 py-6 w-83 h-60 flex flex-col justify-between items-center text-center"
+            onClick={(e) => e.stopPropagation()}
+            >
+                <h2 className="h2-bold text-black">ログインが必要です。</h2>
+                <p className="p text-black text-start">全ての機能を利用するにはログインが必要です。</p>
+                <div className="flex justify-between w-full">
+                    <button onClick={props.onClose}>
+                        <p className="p text-black bg-orange rounded-3 p-2 w-33 h-10 block">このまま続ける</p>
+                    </button>
+                    <button>
+                        <a href="/login" className="bg-blue text-white rounded-3 p-2 p w-33 h-10 block">ログインする</a>
+                    </button>
+                </div>
             </div>
         </div>
     )
